417d300f7542d64c30314aca48938728c3b298d7
Irina Kirillova <keira.kirillova@gmail.com>
2023-07-07 14:55:31 +0200

    add skeleton toproducts cards

---
+++ src/components/CardSkeleton/CardSkeleton.jsx
@@ -0,0 +1,29 @@
+import ContentLoader from 'react-content-loader';
+import styles from './CardSkeleton.module.scss';
+
+const CardSkeleton = () => (
+    <ContentLoader
+        className={styles.cardSkeleton}
+        speed={2}
+        width={400}
+        height={500}
+        viewBox="0 0 400 500"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb">
+        <rect x="0" y="0" rx="0" ry="0" width="100%" height="300" />
+        <rect x="0" y="310" rx="0" ry="0" width="20%" height="20" />
+        <rect x="60%" y="310" rx="0" ry="0" width="40%" height="20" />
+        <rect x="0" y="340" rx="0" ry="0" width="100%" height="20" />
+        <rect x="0" y="370" rx="0" ry="0" width="20%" height="10" />
+        <rect x="0" y="390" rx="0" ry="0" width="48%" height="20" />
+        <rect x="52%" y="390" rx="0" ry="0" width="48%" height="20" />
+        <rect x="0" y="420" rx="0" ry="0" width="20%" height="10" />
+        <rect x="0" y="440" rx="0" ry="0" width="30.6%" height="20" />
+        <rect x="34.6%" y="440" rx="0" ry="0" width="30.6%" height="20" />
+        <rect x="68.6%" y="440" rx="0" ry="0" width="30.6%" height="20" />
+        <rect x="0" y="470" rx="0" ry="0" width="55%" height="30" />
+        <rect x="59%" y="470" rx="0" ry="0" width="41%" height="30" />
+    </ContentLoader>
+);
+
+export default CardSkeleton;
--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from 'react';
 import Categories from '../../components/Categories/Categories';
 import Sort from '../../components/Sort/Sort';
+import CardSkeleton from '../../components/CardSkeleton/CardSkeleton';
 import Card from '../../components/Card/Card';
 import categories from '../../assets/categories.json';
 import styles from './Catalog.module.scss';
 
 const Catalog = () => {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch('https://64a2eabcb45881cc0ae5e05e.mockapi.io/products')
-            .then((data) => data.json())
-            .then((items) => setProducts(items));
+            .then((res) => res.json())
+            .then((data) => {
+                setProducts(data);
+                setLoading(false);
+            });
     }, []);
 
     return (
@@ -22,9 +27,11 @@ const Catalog = () => {
                 <Sort />
             </div>
             <div className={styles.catalog}>
-                {products.map((product) => (
-                    <Card key={product.id} categories={categories} data={product} />
-                ))}
+                {loading
+                    ? [...Array(6)].map((_, i) => <CardSkeleton key={i} />)
+                    : products.map((product) => (
+                          <Card key={product.id} categories={categories} data={product} />
+                      ))}
             </div>
         </>
     );
