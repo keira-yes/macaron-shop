c4c0ed7e89b284335c0dcb8fd298b2d72fde0cbe
Irina Kirillova <keira.kirillova@gmail.com>
2023-08-08 17:32:01 +0200

    add tsconfig

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
         <BrowserRouter>
