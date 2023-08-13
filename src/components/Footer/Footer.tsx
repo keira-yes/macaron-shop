33c2686570f7b3c035f9445af1aefacbf4d5a3c1
Irina Kirillova <keira.kirillova@gmail.com>
2023-08-13 15:15:45 +0200

    add footer content

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,11 @@
+import styles from './Footer.module.scss';
+
 export const Footer: React.FC = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className={styles.footer}>
+            <div className="container">
+                <strong>Macaron Shop 2023 &copy; All right reserved.</strong>
+            </div>
+        </footer>
+    );
 };
